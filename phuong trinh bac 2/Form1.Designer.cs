afc842406e7fabb699b715b5bb57e9e13090e7c2
unknown <ngoquocviet10a3@gmail.com>
2023-03-11 17:56:46 +0700

    first commit

---
+++ phuong trinh bac 2/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace phuong_trinh_bac_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.btntinh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(631, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phương trình bậc 2";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(724, 116);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ax^2+Bx=0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 280);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 204);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nhập A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(243, 364);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nhập C";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(243, 452);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Kết Quả";
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(447, 204);
+            this.txta.Multiline = true;
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(381, 45);
+            this.txta.TabIndex = 0;
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(447, 449);
+            this.txtkq.Multiline = true;
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(673, 146);
+            this.txtkq.TabIndex = 7;
+            this.txtkq.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(447, 364);
+            this.txtc.Multiline = true;
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(381, 44);
+            this.txtc.TabIndex = 2;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(447, 280);
+            this.txtb.Multiline = true;
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(381, 49);
+            this.txtb.TabIndex = 1;
+            // 
+            // btntinh
+            // 
+            this.btntinh.Location = new System.Drawing.Point(996, 204);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(203, 214);
+            this.btntinh.TabIndex = 4;
+            this.btntinh.Text = "Tính";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 872);
+            this.Controls.Add(this.btntinh);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Button btntinh;
+    }
+}
+
---
+++ phuong trinh bac 2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phuong_trinh_bac_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c, dt, x1, x2;
+            string kq = "";
+            a= Convert.ToDouble(txta.Text);
+            b = Convert.ToDouble(txtb.Text);
+            c = Convert.ToDouble(txtc.Text);
+            if (a==0)
+            {
+                if(b==0)
+                {
+                    if (c == 0)
+                    {
+                        kq = "phương trình có vô số nghiệm ";
+
+                    }
+                    else //c khac 0
+                    {
+                        kq = "phương trình vô nghiệm ";
+                    }
+                }
+                else //b khac 0
+                {
+                    kq = string.Format("phương trình có nghiệm là {0)", ((-c) / b));
+                }
+            }
+            else //a khac 0
+            {
+                dt = b * b - 4 * a * c;
+                if (dt < 0)
+                {
+                    kq = "phương trình vô nghiệm";
+
+                }
+                else if (dt==0)
+                {
+                    kq = string.Format("Phương trình có nghiệm kép là {0} ",(((-b) /2* a )));    
+                }
+                else //dt >0
+                {
+                    x1 = (-b + Math.Sqrt(dt)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(dt)) / (2 * a);
+                    kq = string.Format("phương trình có 2 nghiệm kép là: x1 = {1}", x1, x2);
+
+                }
+            }
+            txtkq.Text = kq;
+
+                   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
